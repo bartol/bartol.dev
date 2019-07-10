95550d22f42dc4ab73aa8abd32a19ea4f4037f09
Bartol Deak <b@bdeak.net>
2019-07-11 00:25:02 +0200

    tags

--- src/components/popular-posts.js
+++ src/components/popular-posts.js
@@ -3,41 +3,45 @@ import { graphql, useStaticQuery } from 'gatsby'
 import { css } from '@emotion/core'
 import PopularPost from './popular-post'
 
-const PopularPosts = () => (
-  // const posts = useStaticQuery(graphql`
-  //   query getPopularPosts {
-  //     googleApiData {
-  //       popularPosts
-  //     }
-  //   }
-  // `)
+const PopularPosts = () => {
+  const posts = useStaticQuery(graphql`
+    query getPopularPosts {
+      googleApiData {
+        popularPosts
+      }
+    }
+  `)
 
-  // const { popularPosts } = posts.googleApiData
+  const { popularPosts } = posts.googleApiData
 
-  // const popularPostsList = popularPosts.map(post => (
-  //   <PopularPost Slug={post[0]} />
-  //   // <li>{post[0]}</li>
-  // ))
-
-  <>
-    <h2
-      css={css`
-        /* margin: 0;
+  const popularPostsList = popularPosts.map((post) => {
+    // if (post[1] >= 10) {
+    //   return <PopularPost Slug={post[0]} />
+    // }
+    if (post[0] !== '/blog/another-post/') {
+      return <PopularPost Slug={post[0]} />
+    }
+  })
+  return (
+    <>
+      <h2
+        css={css`
+          /* margin: 0;
           padding-bottom: 0; */
-        /* padding-left: 1px; */
-        border-bottom: none;
-      `}
-    >
-      Popular posts
-    </h2>
-    <ul
-      css={css`
-        padding-left: 1px;
-      `}
-    >
-      {/* {popularPostsList} */}
-    </ul>
-  </>
-)
-
+          /* padding-left: 1px; */
+          border-bottom: none;
+        `}
+      >
+        Popular posts
+      </h2>
+      <ul
+        css={css`
+          padding-left: 1px;
+        `}
+      >
+        {popularPostsList}
+      </ul>
+    </>
+  )
+}
 export default PopularPosts

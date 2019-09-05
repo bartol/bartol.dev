821b496408585a0aee3dece6573ec358a22fd368
bartol <b@bartol.dev>
2019-09-05 21:24:38 +0200

    temp. fix for failing build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import LastPosts from '../components/last-posts'
+// import LastPosts from '../components/last-posts'
 import PopularPosts from '../components/popular-posts'
 import Newsletter from '../components/newsletter-card'
 
@@ -9,7 +9,7 @@ export default () => (
     <h1>Bartol's Coding Blog</h1>
     <p>
       Hey! I'm Bartol Deak, High School student currently living in
-      {' '}
+{' '}
       <a
         href="https://www.google.com/maps/place/Zadar/"
         target="_blank"
@@ -18,7 +18,7 @@ export default () => (
         Zadar
       </a>
       ,
-      {' '}
+{' '}
       <a
         href="https://www.google.com/maps/place/Croatia/"
         target="_blank"
@@ -26,10 +26,10 @@ export default () => (
       >
         Croatia
       </a>
-      . Started learning web development last year and these days I am writing blog posts about
-      technologies, concepts and tools I learn.
+      . Started learning web development last year and these days I am writing
+      blog posts about technologies, concepts and tools I learn.
     </p>
-    <LastPosts />
+    {/* <LastPosts /> */}
     <PopularPosts />
     <Newsletter />
   </Layout>

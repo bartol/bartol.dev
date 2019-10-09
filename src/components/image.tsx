a4bc9ebc6a9cbd841de11eec571e77d162d9fc64
Bartol Deak <b@bdeak.net>
2019-10-09 20:40:53 +0200

    patched last failing build

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
 
 interface Props {
   name: string
@@ -7,31 +6,10 @@ interface Props {
 }
 
 const Image: React.FC<Props> = ({ name, env, className }) => {
-  const data = useStaticQuery(graphql`
-    {
-      allFile(filter: { sourceInstanceName: { eq: "images" } }) {
-        nodes {
-          publicURL
-          name
-        }
-      }
-    }
-  `)
-
-  const { nodes } = data.allFile
-
-  const result = nodes.filter((node: any) => {
-    return node.name === name
-  })
-
-  const url = result[0].publicURL
-  const alt = `${result[0].name} icon`
-
-  // @ts-ignore
   return (
     <img
-      src={url}
-      alt={alt}
+      src={`/images/${name}.svg`}
+      alt={`${name} icon`}
       loading='lazy'
       className={`image ${env} ${className}`}
     />

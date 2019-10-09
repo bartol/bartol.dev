02ffd9069e1ad7cd6369330e7d71cb068f11e50e
Bartol Deak <b@bdeak.net>
2019-10-10 00:18:25 +0200

    sidebar component

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -3,24 +3,26 @@ import Card from './card'
 
 const List = ({ articles }: Article[]) => {
   return (
-    <ul className='flex flex-wrap md:py-3 py-2 xl:px-0 lg:px-1'>
-      {articles.map((article, index) => {
-        const { id, frontmatter, fields } = article
-        const { title, date, tags } = frontmatter
-        const { slug } = fields
-        return (
-          <Card
-            id={id}
-            title={title}
-            date={date}
-            slug={slug}
-            imageName={tags[0]}
-            first={index === 0}
-            key={id}
-          />
-        )
-      })}
-    </ul>
+    <main className='flex flex-col lg:w-4/5 xl:w-5/6'>
+      <ul className='flex flex-wrap md:py-3 py-2 xl:px-0 lg:px-1'>
+        {articles.map((article, index) => {
+          const { id, frontmatter, fields } = article
+          const { title, date, tags } = frontmatter
+          const { slug } = fields
+          return (
+            <Card
+              id={id}
+              title={title}
+              date={date}
+              slug={slug}
+              imageName={tags[0]}
+              first={index === 0}
+              key={id}
+            />
+          )
+        })}
+      </ul>
+    </main>
   )
 }
 

aaa1c9321c439e8cbf5b7c1e0dedfe532fd3013b
Bartol Deak <b@bdeak.net>
2019-10-10 00:16:43 +0200

    skip to article moved to separate component

---
+++ src/components/sidebar/skip.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Skip = () => {
+  return (
+    <button
+      type='button'
+      className='fixed top-0 left-0 px-3 py-2 m-6 bg-gray-900 rounded focus:outline-none focus:shadow-outline-gray-200 z-20 skipToArticles'
+      // eslint-disable-next-line no-undef
+      onClick={() => document.getElementById('focusFirstCard').focus()}
+    >
+      Skip to articles
+    </button>
+  )
+}
+
+export default Skip

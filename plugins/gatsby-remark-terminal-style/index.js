47a69b5d2f815537610ba9cd344b670ec8dd4575
Bartol Deak <b@bdeak.net>
2019-10-10 15:32:51 +0200

    feat: add custom style for terminal

---
+++ plugins/gatsby-remark-terminal-style/index.js
@@ -0,0 +1,25 @@
+const visit = require('unist-util-visit')
+
+module.exports = ({ markdownAST }) => {
+  visit(markdownAST, 'code', (node, index, parent) => {
+    if (node.lang !== 'terminal') return
+
+    const terminalNode = {
+      type: 'html',
+      value: `
+<div class="terminalHeader">
+  <span class="red transition-slow"></span>
+  <span class="yellow transition-slow"></span>
+  <span class="green transition-slow"></span>
+</div>
+        `.trim(),
+    }
+
+    parent.children.splice(index, 0, terminalNode)
+
+    // eslint-disable-next-line no-param-reassign
+    node.lang = 'bash'
+  })
+
+  return markdownAST
+}

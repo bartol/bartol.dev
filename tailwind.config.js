317d4a22eb67a4ece9b4738fac5a45616423d48f
bartol <b@bartol.dev>
2019-10-17 20:47:25 +0200

    feat: add code colors to tailwind palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   theme: {
     extend: {
+      fontFamily: {
+        mono: ['Dank Mono', 'monospace'],
+      },
       colors: {
         main: 'hsla(190, 80%, 50%, 1)',
         dark: {
@@ -13,6 +16,16 @@ module.exports = {
           '300': '#FFFFFFCC', //  80%      atonic
           '200': '#FFFFFFE6', //  90%      text
           '100': '#FFFFFF', //             bold
+        },
+        code: {
+          red: '#ef5350',
+          cyan: '#09d1e9',
+          teal: '#247d8f',
+          gray: '#596774',
+          white: '#cbccc5',
+          green: '#ade96e',
+          yellow: '#ffd470',
+          orange: '#ffa345',
         },
       },
     },

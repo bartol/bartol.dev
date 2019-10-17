b8fc344f1b4b5153aa4b2b0c2ac4c316f3df6800
bartol <b@bartol.dev>
2019-10-17 21:55:55 +0200

    feat: tags are now categories and only 1 can be selected

--- src/components/filter/categories.tsx
+++ src/components/filter/categories.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import tags from './tags'
+import React, { useContext } from 'react'
+import { FilterContext } from '../../state'
+import { categoryBackgroundColor } from './categoryBackgroundColor'
+import list from './tags'
 
 const Categories = () => {
+  const { category, setCategory } = useContext(FilterContext)
+
   return (
     <ul className='sticky top-0'>
-      {tags.map(tag => {
+      {list.map(tag => {
         const { value, label } = tag
         return (
           <li key={value}>
             <button
               type='button'
               value={value}
-              // onClick={() => handleTag(value)}
-              className='w-full text-left px-2 py-1 my-1 rounded text-dark-300 focus:outline-none'
+              onClick={() =>
+                category === value ? setCategory('') : setCategory(value)
+              }
+              className={`w-full text-left px-2 py-1 my-px rounded focus:outline-none transition-fast ${
+                category === value
+                  ? 'bg-dark-700 text-dark-100 font-medium shadow'
+                  : 'text-dark-300'
+              }`}
+              style={{
+                backgroundColor:
+                  category === value ? categoryBackgroundColor(value) : null,
+              }}
             >
               {label}
             </button>
--- src/state/filter/stateToUrl.ts
+++ src/state/filter/stateToUrl.ts
@@ -1,23 +1,22 @@
 import queryString from 'query-string'
 
 export const stateToUrl = (location = { pathname: '/' }, params) => {
-  const { query, tags, sort } = params
+  const { query, category, sort } = params
   const state = {}
 
   if (query) {
     state.q = query
   }
-  if (tags.length) {
-    state.tags = tags
+  if (category) {
+    state.category = category
   }
   if (sort !== 'recent') {
     state.sort = sort
   }
 
   return `${location.pathname}${
-    state.q || state.tags || state.sort ? '?' : ''
+    state.q || state.category || state.sort ? '?' : ''
   }${queryString.stringify(state, {
-    arrayFormat: 'comma',
     sort: false,
   })}`
 }

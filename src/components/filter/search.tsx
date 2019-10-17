89ba8f41401006bcca555546825de7dc8a1c99f1
bartol <b@bartol.dev>
2019-10-17 21:56:14 +0200

    feat: search box wire up

--- src/components/filter/search.tsx
+++ src/components/filter/search.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { FilterContext } from '../../state'
 
 const Search = () => {
+  const { query, setQuery } = useContext(FilterContext)
+
   return (
     <input
-      type='text'
+      type='search'
       placeholder='Search'
+      value={query}
+      onChange={e => setQuery(e.target.value)}
       className='w-full text-2xl bg-dark-700 px-2 my-2 rounded shadow focus:outline-none focus:shadow-outline-dark-200 placeholder-dark-400'
       aria-label='search'
     />

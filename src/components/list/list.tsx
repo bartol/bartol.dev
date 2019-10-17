f12931358bf24a41e8ff81c7c16b3a4a62219a92
Bartol Deak <b@bdeak.net>
2019-10-17 21:58:04 +0200

    feat: handle queries without results

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { FilterContext } from '../../state'
 import Card from './card'
+import NoResults from './noResults'
 
 const List = ({ articles }: Article[]) => {
+  const { results, query } = useContext(FilterContext)
+
   return (
     <main className='flex flex-col lg:w-4/5 xl:w-5/6'>
       <ul className='flex flex-wrap md:py-3 py-2 xl:px-0 lg:px-1'>
         {articles.map((article, index) => {
-          const { id, frontmatter, fields } = article
+          const { id, frontmatter, fields } = article.item || article
           const { title, date, tags } = frontmatter
           const { slug } = fields
           return (
@@ -15,13 +19,14 @@ const List = ({ articles }: Article[]) => {
               title={title}
               date={date}
               slug={slug}
-              imageName={tags[0]}
+              tags={tags}
               first={index === 0}
               key={id}
             />
           )
         })}
       </ul>
+      {!results.length && query ? <NoResults /> : null}
     </main>
   )
 }

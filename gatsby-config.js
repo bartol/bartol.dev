537c0ddbdc6ea5691835bc582692cb99b3767097
bartol <b@bartol.dev>
2019-10-23 19:18:05 +0200

    feat: headings & autolink styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,10 +69,10 @@ module.exports = {
             resolve: 'gatsby-remark-autolink-headers',
             options: {
               icon:
-                '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>',
-              className: 'header-autolink',
+                '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg>',
+              className: 'autolink',
               maintainCase: false,
-              offsetY: '30',
+              offsetY: '20',
             },
           },
           {

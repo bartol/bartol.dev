6a1047ee4ecc13dce4b1a449b6a034d913cdd5c7
Bartol Deak <b@bdeak.net>
2019-10-24 22:25:49 +0200

    feat: add resources component

---
+++ src/components/article/resources.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ManualAutolink } from './manualAutolink'
+
+export const Resources = ({ resources }) => {
+  return (
+    <div className='markdown'>
+      <h3 id='resources'>
+        <ManualAutolink id='resources' />
+        Resources
+      </h3>
+      <ul>
+        {resources.map(resource => {
+          const { name, url } = resource
+          return (
+            <li>
+              <a href={url} target='_blank' rel='nofollow noopener noreferrer'>
+                {name}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}

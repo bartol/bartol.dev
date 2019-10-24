dd4a800c466a34d3f2b7c605d10d1fcf3f214d05
bartol <b@bartol.dev>
2019-10-24 21:56:25 +0200

    chore: move article card to separate component

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,33 +1,21 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import './article.css'
-import { cardBackgroundColor } from '../list/cardBackgroundColor'
+import { Card } from './card'
 
 const Article = ({ data }: Props) => {
   const { markdownRemark } = data
   const { html, frontmatter } = markdownRemark
-  const { title, date, updated, tags } = frontmatter
+  const { title, date, changelog, tags } = frontmatter
 
   return (
     <div className='container max-w-2xl w-90 mx-auto pt-12'>
-      <div
-        className='relative overflow-hidden py-4 px-6 rounded mb-8 flex flex-col justify-between shadow bg-dark-700 articleCard'
-        style={{
-          backgroundColor: cardBackgroundColor(tags[0]),
-        }}
-      >
-        <h1 className='text-5xl leading-tight font-medium'>{title}</h1>
-        <div className='text-xl text-dark-300'>
-          <p>Published on {date}</p>
-          {updated ? <p>Updated on {updated}</p> : null}
-        </div>
-        <img
-          src={`/images/${tags[0]}.svg`}
-          alt={`${tags[0]} icon`}
-          loading='lazy'
-          className='w-64 h-64 opacity-25 absolute bottom-0 right-0 drag-none cardImage transition-slow'
-        />
-      </div>
+      <Card
+        title={title}
+        date={date}
+        updated={changelog[changelog.length - 1].date}
+        tag={tags[0]}
+      />
       {/* eslint-disable-next-line react/no-danger */}
       <div className='markdown' dangerouslySetInnerHTML={{ __html: html }} />
     </div>
@@ -44,6 +32,10 @@ export const query = graphql`
         title
         date(formatString: "MMMM Do, YYYY")
         tags
+        changelog {
+          date(formatString: "MMMM Do, YYYY")
+          message
+        }
       }
     }
   }
@@ -56,9 +48,14 @@ interface Props {
       frontmatter: {
         title: string
         date: string
-        updated: string
         tags: string[]
+        changelog: Changelog[]
       }
     }
   }
+}
+
+interface Changelog {
+  date: string
+  message: string
 }

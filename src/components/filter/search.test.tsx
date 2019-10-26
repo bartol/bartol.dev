cae369f615623c39bc826a1d78368118df85f428
Bartol Deak <b@bdeak.net>
2019-10-26 13:00:02 +0200

    chore: remove jest demo jest test

--- src/components/filter/search.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { render } from '@testing-library/react'
-import Search from './search'
-
-test('search works', () => {
-  const { debug } = render(<Search />)
-  debug()
-})

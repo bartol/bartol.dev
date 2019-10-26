eb6094016e17b5b8a2def1eb3cfb463ec8e3845e
bartol <b@bartol.dev>
2019-10-27 01:35:32 +0200

    fix: lint staged was adding typo - now disabled on md files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '*.{js,jsx,ts,tsx}': [
     'eslint', // lint JavaScript and TypeScript
   ],
-  '*.{js,jsx,json,yml,yaml,css,less,scss,ts,tsx,md,mdx,graphql}': [
+  '*.{js,jsx,json,yml,yaml,css,less,scss,ts,tsx,graphql}': [
     'prettier --write', // run prettier and fix errors
     'git add', // add fixed files to next commit
   ],

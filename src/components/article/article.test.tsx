07b9a0f355c6890b1ec1ed112f7a361948a5ee15
Bartol Deak <b@bdeak.net>
2019-10-27 17:53:18 +0100

    feat: add tests for article

---
+++ src/components/article/article.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { toBeInTheDocument } from '@testing-library/jest-dom'
+import Article from './article'
+
+expect.extend({ toBeInTheDocument })
+
+describe('article', () => {
+  // mock article
+  const data = {
+    markdownRemark: {
+      html: '<p>This is article body</p>',
+      frontmatter: {
+        title: 'Article title',
+        date: 'Oct 27th, 2019',
+        tags: ['typescript'],
+      },
+    },
+  }
+
+  it('renders title', () => {
+    const { getByText } = render(<Article data={data} />)
+
+    expect(getByText(/Article title/i)).toBeInTheDocument()
+  })
+
+  it('renders body', () => {
+    const { getByText } = render(<Article data={data} />)
+
+    expect(getByText(/This is article body/i)).toBeInTheDocument()
+  })
+
+  it('renders date', () => {
+    const { getByText } = render(<Article data={data} />)
+
+    expect(getByText(/Oct 27th, 2019/i)).toBeInTheDocument()
+  })
+})

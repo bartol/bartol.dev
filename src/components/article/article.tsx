3d997a08d018546638aa7a014235679135c8e70b
Bartol Deak <b@bdeak.net>
2019-11-05 21:07:11 +0100

    feat: edit article

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,5 +1,10 @@
-import React from 'react'
+/* eslint no-alert:0 */
+/* eslint no-undef:0 */
+import React, { useState } from 'react'
 import { graphql } from 'gatsby'
+import Textarea from 'react-autosize-textarea'
+import { request } from 'graphql-request'
+import { diffSentences } from 'diff'
 import './article.css'
 import './code.css'
 import { Card } from './card'
@@ -7,12 +12,52 @@ import { Changelog } from './changelog'
 import { Resources } from './resources'
 import SEO from '../seo'
 
+const mutation = `
+mutation SendEdit($title: String!, $body: String!, $name: String, $email: String) {
+  edit(title: $title, body: $body, name: $name, email: $email)
+}`
+
 const Article = ({ data }: Props) => {
   const { markdownRemark } = data
   const { html, frontmatter, fields, thumbnail } = markdownRemark
+  let { rawMarkdownBody } = markdownRemark
   const { title, date, changelog, resources, tags } = frontmatter
   const { slug } = fields
   const { publicURL: thumbnailURL } = thumbnail
+
+  if (rawMarkdownBody.startsWith('\n'))
+    rawMarkdownBody = rawMarkdownBody.slice(1)
+
+  if (rawMarkdownBody.endsWith('\n'))
+    rawMarkdownBody = rawMarkdownBody.slice(0, -1)
+
+  const [edit, setEdit] = useState(false)
+  const [rawMarkdown, setRawMarkdown] = useState(rawMarkdownBody)
+
+  const send = async () => {
+    if (rawMarkdownBody === rawMarkdown) {
+      alert('Please edit article to send it.')
+      return
+    }
+
+    const body = JSON.stringify(diffSentences(rawMarkdownBody, rawMarkdown))
+
+    const variables = {
+      title,
+      body,
+    }
+
+    const res = await request('https://api.bartol.dev', mutation, variables)
+    if (res.edit === 'success') {
+      alert('Edit sent successfuly.')
+
+      setEdit(false)
+    } else {
+      alert(
+        'Oops! There was problem transfering your knowledge. Please try again or email me contact@bartol.dev'
+      )
+    }
+  }
 
   return (
     <div className='container max-w-2xl w-90 mx-auto pt-12'>
@@ -28,10 +73,43 @@ const Article = ({ data }: Props) => {
         updated={changelog ? changelog[changelog.length - 1].date : null}
         tag={tags[0]}
       />
-      {/* eslint-disable-next-line react/no-danger */}
-      <div className='markdown' dangerouslySetInnerHTML={{ __html: html }} />
+      {edit ? (
+        <Textarea
+          className='rawMarkdown'
+          aria-label='edit article'
+          value={rawMarkdown}
+          onChange={e => setRawMarkdown(e.target.value)}
+        />
+      ) : (
+        /* eslint-disable-next-line react/no-danger */
+        <div className='markdown' dangerouslySetInnerHTML={{ __html: html }} />
+      )}
       {resources ? <Resources resources={resources} /> : null}
       {changelog ? <Changelog changelog={changelog} /> : null}
+      <div className='hidden md:flex fixed bottom-0 right-0 mb-6 mr-6 flex-col items-end'>
+        {edit && rawMarkdownBody !== rawMarkdown ? (
+          <button
+            className='block rounded-full bg-green-700 px-6 py-3 shadow m-2'
+            type='button'
+            onClick={() => send()}
+          >
+            Send edit
+          </button>
+        ) : null}
+        <button
+          className='block rounded-full bg-dark-700 px-6 py-3 shadow m-2'
+          style={{ backgroundColor: edit ? '#cc3737' : null }}
+          type='button'
+          onClick={() => {
+            setEdit(!edit)
+            if (edit) {
+              setRawMarkdown(rawMarkdownBody)
+            }
+          }}
+        >
+          {edit ? 'Discard edit' : 'Edit article'}
+        </button>
+      </div>
     </div>
   )
 }
@@ -42,6 +120,7 @@ export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      rawMarkdownBody
       frontmatter {
         title
         date(formatString: "MMMM Do, YYYY")

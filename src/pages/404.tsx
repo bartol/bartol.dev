9398e238cb4767927ee793d9895f69a8b4606088
Bartol Deak <b@bdeak.net>
2019-11-11 19:35:36 +0100

    feat: new 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import SEO from '../components/seo'
-
-const emojis = ['(>_<)', '(·.·)', 'ಠ_ಠ', '(⊙_☉)', '(~_~;)']
-
-const current = Math.floor(new Date().getSeconds() / 12)
+import './404.css'
 
 export default () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto h-screen flex flex-col justify-center items-center -mt-12'>
       <SEO title='Oops' url='/404/' isArticle={false} />
-      <div className='ascii'>{emojis[current]}</div>
-      <div className='notFoundMessage'>
-        <h1>404</h1>
+      <div className='ascii leading-snug font-mono hover:text-main transition-slow'>
+        (·.·)
+      </div>
+      <div className='flex mt-1'>
+        <h1 className='text-5xl leading-none mr-3'>404</h1>
         <section>
           <h2>Seems like you are lost.</h2>
           <h2>
-            Let me bring you <Link to='/'>home</Link>.
+            Let me bring you{' '}
+            <Link to='/' className='link'>
+              home
+            </Link>
+            .
           </h2>
         </section>
       </div>

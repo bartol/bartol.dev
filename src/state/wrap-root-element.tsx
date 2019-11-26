54a2ab019979c8a495d96da92b0daf2783cc40cd
Bartol Deak <b@bdeak.net>
2019-11-26 13:41:23 +0100

    chore: removed almost everything

--- src/state/wrap-root-element.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { ContextProvider } from './provider-composer'
-
-export const wrapRootElement = ({ element }) => {
-  return <ContextProvider>{element}</ContextProvider>
-}

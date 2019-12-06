5a7af0fa656aac26210c16d15ffdca9702c86574
Bartol Deak <b@bdeak.net>
2019-12-06 23:11:38 +0100

    feat: basic search

--- js/search.js
+++ js/search.js
@@ -3,11 +3,21 @@ const results = document.getElementById('results')
 
 async function handle_search() {
     const { posts } = await fetch('/js/search.json').then(res => res.json())
-    console.log('posts:', posts)
+
+    const options = {
+        keys: ['title', 'category', 'url']
+    };
+
+    const fuse = new Fuse(posts, options)
 
     search_box.addEventListener('keyup', e => {
         const query = e.target.value
-        console.log('query:', query)
+        const res = fuse.search(query).slice(0, 5)
+
+        results.innerHTML = res.map(item => {
+            const { title, category, url } = item
+            return `<li class="result ${category}"><a href="${url}">${title}</a></li>`
+        }).join('')
     })
 }
 

ebacdd4f42d76c1a3ce00a456431eaf5740fef12
Bartol Deak <b@bdeak.net>
2019-12-10 23:25:09 +0100

    fix: search.js indentation

--- js/search.js
+++ js/search.js
@@ -2,23 +2,25 @@ const search_box = document.getElementById('search')
 const results = document.getElementById('results')
 
 async function handle_search() {
-    const { posts } = await fetch('/js/search.json').then(res => res.json())
+  const { posts } = await fetch('/js/search.json').then(res => res.json())
 
-    const options = {
-        keys: ['title', 'category', 'url']
-    };
+  const options = {
+    keys: ['title', 'category', 'url'],
+  }
 
-    const fuse = new Fuse(posts, options)
+  const fuse = new Fuse(posts, options)
 
-    search_box.addEventListener('keyup', e => {
-        const query = e.target.value
-        const res = fuse.search(query).slice(0, 5)
+  search_box.addEventListener('keyup', e => {
+    const query = e.target.value
+    const res = fuse.search(query).slice(0, 5)
 
-        results.innerHTML = res.map(item => {
-            const { title, category, url } = item
-            return `<li class="result ${category}"><a href="${url}">${title}</a></li>`
-        }).join('')
-    })
+    results.innerHTML = res
+      .map(item => {
+        const { title, category, url } = item
+        return `<li class="result ${category}"><a href="${url}">${title}</a></li>`
+      })
+      .join('')
+  })
 }
 
 handle_search()

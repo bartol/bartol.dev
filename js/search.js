1ebd1cd0db89b9a2695d5304c288eb3c1f8b315e
bartol <b@bartol.dev>
2019-12-15 20:22:20 +0100

    Search dropdown usable (only with mouse)

--- js/search.js
+++ js/search.js
@@ -10,17 +10,80 @@ async function handle_search() {
 
   const fuse = new Fuse(posts, options)
 
-  search_box.addEventListener('keyup', e => {
+  const search = e => {
     const query = e.target.value
-    const res = fuse.search(query).slice(0, 5)
+    const old_query = search_box.getAttribute('data-query')
 
-    results.innerHTML = res
-      .map(item => {
-        const { title, category, url } = item
-        return `<li class="result ${category}"><a href="${url}">${title}</a></li>`
-      })
-      .join('')
+    if (query !== old_query) {
+      search_box.setAttribute('data-query', query)
+
+      const res = fuse.search(query).slice(0, 5)
+
+      results.innerHTML = res
+        .map(item => {
+          const { title, category, url } = item
+          return `<li class="result ${category}"><a href="${url}">${title}</a></li>`
+        })
+        .join('')
+
+      if (results.children.length) results.firstChild.classList.add('selected')
+    }
+  }
+
+  search_box.addEventListener('focus', e => {
+    if (results.children.length) {
+      results.classList.remove('hidden')
+      return
+    }
+
+    if (e.target.value) search(e)
+  })
+
+  search_box.addEventListener('blur', () => {
+    if (results.children.length) {
+      results.classList.add('hidden')
+    }
+  })
+
+  search_box.addEventListener('keyup', e => {
+    search(e)
   })
 }
 
 handle_search()
+
+window.addEventListener('keyup', e => {
+  const { keyCode, shiftKey, ctrlKey } = e
+
+  const is_tab = keyCode === 9 && !shiftKey
+  const is_shift_tab = keyCode === 9 && shiftKey
+  const is_arrow_down = keyCode === 40
+  const is_arrow_up = keyCode === 38
+  const is_ctrl_j = keyCode === 74 && ctrlKey
+  const is_ctrl_k = keyCode === 75 && ctrlKey
+  const is_enter = keyCode === 13
+
+  const selected = document.getElementsByClassName('selected')[0] || {}
+  const next = selected.nextElementSibling
+  const prev = selected.previousElementSibling
+
+  if (next) {
+    if (is_tab || is_arrow_down || is_ctrl_j) {
+      selected.classList.remove('selected')
+      next.classList.add('selected')
+      search_box.focus()
+    }
+  }
+
+  if (prev) {
+    if (is_shift_tab || is_arrow_up || is_ctrl_k) {
+      selected.classList.remove('selected')
+      prev.classList.add('selected')
+      search_box.focus()
+    }
+  }
+
+  if (is_enter) {
+    selected.firstChild.click()
+  }
+})

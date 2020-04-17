c712ee9d4987823a70ca6c345b6198210fda929c
Bartol Deak <b@bdeak.net>
2020-04-17 23:39:42 +0200

    TIL

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,27 @@
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addCollection('post', function(collection) {
+  eleventyConfig.addCollection('blog', function(collection) {
     return collection.getAll().filter(function(item) {
-      return item.data.layout === 'post'
+      return item.data.layout === 'blog'
     })
   })
+
+  eleventyConfig.addCollection('til', function(collection) {
+    return collection.getAll().filter(function(item) {
+      return item.data.layout === 'til'
+    })
+  })
+
+  // eleventyConfig.addCollection('file', function(collection) {
+
+  // })
 
   eleventyConfig.addFilter('formatDate', function(date) {
     return date.toISOString().substring(0, 10)
   })
 
   eleventyConfig.addPassthroughCopy("posts");
+  eleventyConfig.addPassthroughCopy("til");
   eleventyConfig.addPassthroughCopy("files");
   eleventyConfig.addPassthroughCopy("css");
 
--- blog/move-line-in-vim.md
+++ blog/move-line-in-vim.md
@@ -1,5 +1,5 @@
 ---
-layout: post
+layout: blog
 title: Move line in Vim
 date: 2019-11-22
 ---

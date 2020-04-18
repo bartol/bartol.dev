be03bf568978d537d2b32fe77fbd9ba39a427d48
bartol <b@bartol.dev>
2020-04-18 22:08:27 +0200

    remove reading

--- .eleventy.js
+++ .eleventy.js
@@ -14,23 +14,12 @@ module.exports = function(eleventyConfig) {
     })
   })
 
-  eleventyConfig.addCollection('reading', function(collection) {
-    return collection.getAll().filter(function(item) {
-      return item.data.layout === 'reading'
-    })
-  })
-
-  // eleventyConfig.addCollection('file', function(collection) {
-
-  // })
-
   eleventyConfig.addFilter('formatDate', function(date) {
     return date.toISOString().substring(0, 10)
   })
 
   eleventyConfig.addPassthroughCopy("blog");
   eleventyConfig.addPassthroughCopy("til");
-  eleventyConfig.addPassthroughCopy("reading");
 
   eleventyConfig.addPassthroughCopy("files");
   eleventyConfig.addPassthroughCopy("css");

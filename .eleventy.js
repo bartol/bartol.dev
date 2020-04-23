c1cb2c434c0118b2665734c8969f80859ccb068c
Bartol Deak <b@bdeak.net>
2020-04-23 22:15:45 +0200

    Til list and rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addCollection('til', function(collection) {
+    return collection.getAll().filter(function(item) {
+      return item.data.type === 'til'
+    })
+  })
+
+  eleventyConfig.addFilter('formatDate', function(date) {
+    return date.toISOString().substring(0, 10)
+  })
+
+  eleventyConfig.addPassthroughCopy("files");
+  eleventyConfig.addPassthroughCopy("css");
+
+  eleventyConfig.addPlugin(pluginRss);
+
   return {
     dir: {
       output: "build",
---
+++ til/first-post.md
@@ -0,0 +1,9 @@
+---
+title: First post
+date: 2020-04-22
+tags:
+  - test
+  - test 2
+---
+
+hey

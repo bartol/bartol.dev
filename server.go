07e29e31b64b941d75aa27f57e873461d92cc257
Bartol Deak <b@bdeak.net>
2020-05-06 16:19:10 +0200

    full rewrite of go server

--- server.go
+++ server.go
@@ -1,168 +1,105 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/russross/blackfriday/v2"
 )
 
-// Verb - Route            - File on server           - Description
-//
-// GET  - /                - ./index.html             - home
-// GET  - /:path.ext       - ./static/:path.ext       - static files
-//
-// GET  - /til             - ./til/index.html         - til index
-// GET  - /til.xml         - ./til.xml                - til index rss
-// GET  - /til/:path       - ./til/:path/index.html   - til subindex
-// GET  - /til/:path       - ./til/:path.html         - til post content
-// GET  - /til/:path.md    - ./til/:path.md           - til post source
-//
-// GET  - /blog            - ./blog/index.html        - blog index
-// GET  - /blog.xml        - ./blog.xml               - blog index rss
-// GET  - /blog/:path      - ./blog/:path/index.html  - blog subindex
-// GET  - /blog/:path      - ./blog/:path.html        - blog post content
-// GET  - /blog/:path.md   - ./blog/:path.md          - blog post source
-//
-// GET  - /files           - ./files/index.html       - files index
-// GET  - /files/:path     - ./files/:path/index.html - files subindex
-// GET  - /files/:path.ext - ./files/:path.ext        - file
-// GET  - /upload          - ./upload.html            - upload files web gui
-// POST - /upload          - n/a                      - upload files
-//
-// GET  - /share           - n/a                      - webrtc file share
-// GET  - /chess           - n/a                      - webrtc chess
-//
-// /ping - pong
+// routes //////////////////////////////////////////////////////////////////////
+
+// GET  - /                - home
+// GET  - /:path.ext       - static files
+
+// GET  - /til             - til index
+// GET  - /til.xml         - til index rss
+// GET  - /til/:path       - til subindex
+// GET  - /til/:path       - til post content
+// GET  - /til/:path.md    - til post source
+
+// GET  - /blog            - blog index
+// GET  - /blog.xml        - blog index rss
+// GET  - /blog/:path      - blog subindex
+// GET  - /blog/:path      - blog post content
+// GET  - /blog/:path.md   - blog post source
+
+// GET  - /files           - files index
+// GET  - /files/:path     - files subindex
+// GET  - /files/:path.ext - file
+// GET  - /upload          - upload files web gui
+// POST - /upload          - upload files
+
+// GET  - /share           - webrtc file share
+// GET  - /chess           - webrtc chess
+
+// GET  - /ping            - pong
 
 func main() {
-	fmt.Println("starting go server")
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Use(middleware.URLFormat)
+	r.Use(middleware.Compress(5))
 
-	// r.Route("/til", func(r chi.Router) {
-	// 	r.Get("/", handler)
-	// 	r.Get("/*", handler)
-	// })
+	r.Get("/", indexHandler)
+	serveStatic(r, "/robots.txt")
 
-	r.Get("/files/*", func(w http.ResponseWriter, r *http.Request) {
-		fs := http.StripPrefix("/files/", http.FileServer(http.Dir("./files")))
-		fs.ServeHTTP(w, r)
-	})
+	redirectSlash(r, "/files")
+	r.Get("/files/*", filesHandler)
+
+	r.Get("/ping", pingHandler)
 
 	r.NotFound(notFoundHandler)
 
-	http.ListenAndServe(":3000", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-// -----------------------------------------------------------------------------
+// helpers /////////////////////////////////////////////////////////////////////
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	urlFormat := getUrlFormat(r)
-
-	switch urlFormat {
-	case "md":
-		path := "." + r.RequestURI
-		if !postExists(path) {
-			notFoundHandler(w, r)
-			return
-		}
-		md, err := ioutil.ReadFile(path)
-		if err != nil {
-			internalServerError(w, r)
-			return
-		}
-		w.Write(md)
-	default:
-		path := "." + r.RequestURI + ".md"
-		if !postExists(path) {
-			notFoundHandler(w, r)
-			return
-		}
-
-		metadata, err := parseMetadata(path)
-		if err != nil {
-			internalServerError(w, r)
-			return
-		}
-
-		md, err := ioutil.ReadFile(path)
-		if err != nil {
-			internalServerError(w, r)
-			return
-		}
-		body := parseMarkdown(md)
-
-		post := Post{metadata, body}
-
-		t, err := template.ParseFiles("templates/post.html")
-		if err != nil {
-			internalServerError(w, r)
-			return
-		}
-
-		t.Execute(w, post)
-	}
+func redirect(r chi.Router, from, to string, method int) {
+	r.Get(
+		from,
+		http.RedirectHandler(to, http.StatusPermanentRedirect).ServeHTTP,
+	)
 }
 
-func getUrlFormat(r *http.Request) string {
-	return r.Context().Value(middleware.URLFormatCtxKey).(string)
+func redirectSlash(r chi.Router, from string) {
+	to := from + "/"
+	redirect(r, from, to, http.StatusMovedPermanently)
 }
 
-func postExists(path string) bool {
-	info, err := os.Stat(path)
-	return err == nil && !info.IsDir() && filepath.Ext(path) == ".md"
+func serve(r chi.Router, src, path string) {
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, src)
+	})
 }
 
-type Post struct {
-	Metadata PostMetadata
-	Body     template.HTML
+func serveStatic(r chi.Router, path string) {
+	src := "./static" + path
+	serve(r, src, path)
 }
 
-type PostMetadata struct {
-	Title string
-	Date  time.Time
-	Size  int64
+// handlers ////////////////////////////////////////////////////////////////////
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("index"))
 }
 
-func parseMarkdown(md []byte) template.HTML {
-	return template.HTML(blackfriday.Run(md))
+func fileHandler(w http.ResponseWriter, r *http.Request) {
+	file := "./static" + chi.RouteContext(r.Context()).RoutePattern()
+	w.Write([]byte(file))
 }
 
-func parseMetadata(path string) (PostMetadata, error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		return PostMetadata{}, err
-	}
+func filesHandler(w http.ResponseWriter, r *http.Request) {
+	fs := http.StripPrefix("/files/", http.FileServer(http.Dir("./files")))
+	fs.ServeHTTP(w, r)
+}
 
-	fn := info.Name()
-	fnWithoutExtension := strings.TrimSuffix(fn, filepath.Ext(fn))
-	fnWithoutDashes := strings.ReplaceAll(fnWithoutExtension, "-", " ")
-	fnWithoutUnderlines := strings.ReplaceAll(fnWithoutDashes, "_", " ")
-	title := strings.Title(fnWithoutUnderlines)
-	date := info.ModTime()
-	size := info.Size()
-
-	return PostMetadata{title, date, size}, nil
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Ping-Pong, Umjetnost Zdravog Đira"))
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(404)
-	w.Write([]byte("nothing here"))
-	// http.NotFound(w, r)
-}
-
-func internalServerError(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(500)
-	w.Write([]byte("internal server error\nplease contact me b@bartol.dev"))
+	w.Write([]byte("404 t"))
 }

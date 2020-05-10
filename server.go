74526a61f762c5f605252cb6294d76002cf14ea5
bartol <b@bartol.dev>
2020-05-10 22:57:23 +0200

    Rewrite server.go with stdlib only

--- server.go
+++ server.go
@@ -4,21 +4,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 )
-
-// routes //////////////////////////////////////////////////////////////////////
-
-// GET  - /                - home
-// GET  - /:path.ext       - static files
-
-// GET  - /til             - til index
-// GET  - /til.xml         - til index rss
-// GET  - /til/:path       - til subindex
-// GET  - /til/:path       - til post content
-// GET  - /til/:path.md    - til post source
 
 // GET  - /blog            - blog index
 // GET  - /blog.xml        - blog index rss
@@ -26,11 +12,13 @@ import (
 // GET  - /blog/:path      - blog post content
 // GET  - /blog/:path.md   - blog post source
 
-// GET  - /files           - files index
-// GET  - /files/:path     - files subindex
-// GET  - /files/:path.ext - file
-// GET  - /upload          - upload files web gui
-// POST - /upload          - upload files
+// GET  - /upload          - upload index
+// POST - /upload          - new upload
+// GET  - /upload/:id      - view upload
+
+// GET  - /paste           - paste index
+// POST - /paste           - new paste
+// GET  - /paste/:id       - view paste
 
 // GET  - /share           - webrtc file share
 // GET  - /chess           - webrtc chess
@@ -38,76 +26,52 @@ import (
 // GET  - /color           - color image api
 // GET  - /random          - generate random number
 
-// GET  - /ping            - pong
-
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.Compress(5))
+	http.HandleFunc("/", indexHandler)
 
-	r.Get("/", indexHandler)
-	serveStatic(r, "/robots.txt")
+	http.HandleFunc("/til/", tilHandler)
+	http.HandleFunc("/til.xml", tilFeedHandler)
 
-	r.Route("/til", func(r chi.Router) {
-		// r.Get("/", tilIndexHandler)
-		r.Get("/{path}", tilPostHandler)
-	})
+	serveFile("/robots.txt")
+	serveDir("/css/")
+	serveDir("/js/")
+	serveDir("/files/")
 
-	redirectSlash(r, "/files")
-	r.Get("/files/*", filesHandler)
+	http.HandleFunc("/ping", pingHandler)
 
-	r.Get("/ping", pingHandler)
-
-	r.NotFound(notFoundHandler)
-
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// utils ///////////////////////////////////////////////////////////////////////
-
-func redirect(r chi.Router, from, to string, method int) {
-	r.Get(
-		from,
-		http.RedirectHandler(to, http.StatusPermanentRedirect).ServeHTTP,
-	)
-}
-
-func redirectSlash(r chi.Router, from string) {
-	to := from + "/"
-	redirect(r, from, to, http.StatusMovedPermanently)
-}
-
-func serve(r chi.Router, src, path string) {
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, src)
-	})
-}
-
-func serveStatic(r chi.Router, path string) {
-	src := "./static" + path
-	serve(r, src, path)
-}
+// middleware //////////////////////////////////////////////////////////////////
 
 // handlers ////////////////////////////////////////////////////////////////////
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	if r.URL.Path != "/" {
+		notFoundHandler(w, r)
+		return
+	}
+
 	w.Write([]byte("index"))
 }
 
-func tilPostHandler(w http.ResponseWriter, r *http.Request) {
-	path := "./til/" + chi.URLParam(r, "path")
+func tilHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[len("/til/"):]
+	if path != "" {
+		if strings.HasSuffix(path, ".md") {
+			w.Write([]byte("md " + path))
+			return
+		}
 
-	if strings.HasSuffix(path, ".md") {
-		w.Write([]byte("md: " + path))
+		w.Write([]byte("html " + path))
 		return
 	}
-	w.Write([]byte("html: " + path))
+
+	w.Write([]byte("til index"))
 }
 
-func filesHandler(w http.ResponseWriter, r *http.Request) {
-	fs := http.StripPrefix("/files/", http.FileServer(http.Dir("./files")))
-	fs.ServeHTTP(w, r)
+func tilFeedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("til feed"))
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
@@ -115,5 +79,18 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("404 t"))
+	w.Write([]byte("404"))
+}
+
+// utils ///////////////////////////////////////////////////////////////////////
+
+func serveFile(path string) {
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "."+path)
+	})
+}
+
+func serveDir(path string) {
+	fs := http.FileServer(http.Dir("." + path))
+	http.Handle(path, http.StripPrefix(path, fs))
 }

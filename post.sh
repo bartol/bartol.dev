9661263e50fbf5d3e73745b58637345b0d02093f
Bartol Deak <b@bdeak.net>
2020-07-18 21:36:10 +0200

    handle , in post titles

--- post.sh
+++ post.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 title=$1
-file=memory/$(echo ${title,,} | sed -E "s/( |\/)/-/g" | sed -E "s/('|\.)//g").md
+file=memory/$(echo ${title,,} | sed -E "s/( |\/)/-/g" | sed -E "s/('|\.|,)//g").md
 
 echo "#" $title > $file
 echo >> $file

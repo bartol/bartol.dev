226e9f30cfcd5a59b6522a178a08ac9f51c27642
Bartol Deak <b@bartol.dev>
2020-07-19 17:10:49 +0200

    post script support path

--- post.sh
+++ post.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 title=$1
-file=memory/$(echo ${title,,} | sed -E "s/( |\/)/-/g" | sed -E "s/('|\.|,)//g").md
+path=$2
+file=memory/$path/$(echo ${title,,} | sed -E "s/( |\/)/-/g" | sed -E "s/('|\.|,)//g").md
 
 echo "#" $title > $file
 echo >> $file

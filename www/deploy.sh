45adff84c307e1a16b3985002a0d30f25a1239a9
Bartol Deak <b@bdeak.net>
2020-09-17 19:20:12 +0200

    deploy script

---
+++ www/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+[ "$1" == "init" ] && ssh srv1 << 'END'
+sudo mkdir -p /home/www-data/www.bdeak.net
+sudo chown -R $USER /home/www-data/www.bdeak.net
+cat << 'EOF' | sudo tee -a /etc/nginx/sites-available/bdeak.net
+server {
+    server_name www.bdeak.net;
+
+	root /home/www-data/www.bdeak.net;
+	autoindex on;
+
+	location / {
+		if ($request_uri ~ ^/(.*)\.html) {
+			return 301 /$1;
+		}
+		try_files $uri $uri.html $uri/ =404;
+	}
+}
+EOF
+sudo ln -s /etc/nginx/sites-{available,enabled}/bdeak.net
+sudo certbot --nginx -n -d www.bdeak.net --redirect
+END
+
+./generate.py
+
+rsync -r --delete out/ srv1:/home/www-data/www.bdeak.net

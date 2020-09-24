c5ada10d4a2e1c137bed3f828db5d15fe0fd09e8
Bartol Deak <b@bdeak.net>
2020-09-24 23:41:52 +0200

    remove init part from deploy script

--- www/deploy.sh
+++ www/deploy.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-[ "$1" == "init" ] && ssh srv1 << 'END'
-sudo mkdir -p /home/www-data/www.bdeak.net
-sudo chown -R $USER /home/www-data/www.bdeak.net
-curl https://www.bdeak.net/nginx-site | sudo tee -a /etc/nginx/sites-available/bdeak.net
-sudo ln -s /etc/nginx/sites-{available,enabled}/bdeak.net
-sudo certbot --nginx -n -d www.bdeak.net -d bdeak.net --redirect
-END
-
 ./generate.py
 
 rsync -r --delete out/ srv1:/home/www-data/www.bdeak.net

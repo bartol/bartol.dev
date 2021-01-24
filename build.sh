9c0a4da3846a46ccfc1a3810c0d687d741a139c9
Bartol Deak <b@bdeak.net>
2021-01-24 19:10:28 +0100

    update build script

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 mkdir out
 cp -r static/* notes index.md out
 for file in $(find out -name "*.md"); do
-  file_out=${file%.md}.html
-  pandoc $file -o $file_out \
-    --template layout.html \
-    --metadata title="$(head -1 $file | cut -c3-)"
+	pandoc $file -o ${file%.md}.html \
+		--template layout.html \
+		--metadata title="$(head -1 $file | cut -c3-)"
 done
 
-rsync -r --delete --stats out/ srv1:/home/www-data/www.bdeak.net
+rsync -r --delete --stats out/ root@srv1.bdeak.net:/home/www-data/www.bdeak.net
 rm -r out
